59fa56875af2c809ecaa5cf12dda303ac43dee4b
XDean <373216024@qq.com>
2019-02-25 19:18:11 +0800

    init environment

---
+++ wechat-mini-programe-boardgame-server/wechat-mini-programe-boardgame-server-app/src/main/java/xdean/wechat/mini/boardgame/server/WechatMiniBoardgameApp.java
@@ -0,0 +1,11 @@
+package xdean.wechat.mini.boardgame.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatMiniBoardgameApp {
+  public static void main(String[] args) {
+    SpringApplication.run(WechatMiniBoardgameApp.class, args);
+  }
+}
