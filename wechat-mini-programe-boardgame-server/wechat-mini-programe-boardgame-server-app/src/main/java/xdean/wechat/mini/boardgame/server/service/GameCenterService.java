bc8f2b398c847c49567bb8dc05f684aac3e5e32d
XDean <373216024@qq.com>
2019-02-27 13:40:02 +0800

    init game center

---
+++ wechat-mini-programe-boardgame-server/wechat-mini-programe-boardgame-server-app/src/main/java/xdean/wechat/mini/boardgame/server/service/GameCenterService.java
@@ -0,0 +1,7 @@
+package xdean.wechat.mini.boardgame.server.service;
+
+public interface GameCenterService {
+  void createGame();
+
+  void joinGame();
+}
---
+++ wechat-mini-programe-boardgame-server/wechat-mini-programe-boardgame-server-games/wechat-mini-programe-boardgame-server-gdjzj/src/main/java/xdean/wechat/mini/boardgame/gdjzj/service/GdjzjFrontEndService.java
@@ -0,0 +1,5 @@
+package xdean.wechat.mini.boardgame.gdjzj.service;
+
+public interface GdjzjFrontEndService {
+
+}
