a37485e9199a6f9d110aa928a03f454b894cbf29
XDean <373216024@qq.com>
2019-02-27 14:17:32 +0800

    test cookie

--- wechat-mini-programe-boardgame-server/wechat-mini-programe-boardgame-server-app/src/main/java/xdean/wechat/mini/boardgame/server/endpoint/SystemEndPoint.java
+++ wechat-mini-programe-boardgame-server/wechat-mini-programe-boardgame-server-app/src/main/java/xdean/wechat/mini/boardgame/server/endpoint/SystemEndPoint.java
@@ -3,9 +3,12 @@ package xdean.wechat.mini.boardgame.server.endpoint;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,17 @@ public class SystemEndPoint {
   @GetMapping("/hello")
   public String hello(@RequestParam(name = "who", defaultValue = "World") String who) {
     return "Hello " + who + "!";
+  }
+
+  @GetMapping("/hello-cookie")
+  public String helloCookie(@CookieValue(name = "who", defaultValue = "World") String who) {
+    return "Hello " + who + "!";
+  }
+
+  @GetMapping("/hello-cookie-set")
+  public String setHelloCookie(HttpServletResponse response, @RequestParam(name = "who", defaultValue = "World") String who) {
+    response.addCookie(new Cookie("who", who));
+    return "Set to " + who + "!";
   }
 
   @GetMapping("/shutdown")
