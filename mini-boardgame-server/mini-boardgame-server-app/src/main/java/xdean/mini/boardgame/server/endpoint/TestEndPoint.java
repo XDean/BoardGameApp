28d2f2a0955d745dd83dba40537f006075551209
XDean <373216024@qq.com>
2019-03-02 14:41:02 +0800

    test flux and SSE

--- mini-boardgame-server/mini-boardgame-server-app/src/main/java/xdean/mini/boardgame/server/endpoint/TestEndPoint.java
+++ mini-boardgame-server/mini-boardgame-server-app/src/main/java/xdean/mini/boardgame/server/endpoint/TestEndPoint.java
@@ -1,8 +1,12 @@
 package xdean.mini.boardgame.server.endpoint;
 
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import reactor.core.publisher.Flux;
+import xdean.mini.boardgame.server.security.model.LoginOpenIdResponse;
 
 @RestController
 @Api(tags = "Test")
@@ -40,5 +46,15 @@ public class TestEndPoint {
   public String setHelloCookie(HttpServletResponse response, @RequestParam(name = "who", defaultValue = "World") String who) {
     response.addCookie(new Cookie("who", who));
     return "Set to " + who + "!";
+  }
+
+  @GetMapping("/flux/ints")
+  public Flux<ServerSentEvent<LoginOpenIdResponse>> ints() {
+    return Flux.interval(Duration.ofSeconds(1))
+        .map(seq -> ServerSentEvent.<LoginOpenIdResponse> builder()
+            .event("random")
+            .id(seq.toString())
+            .data(LoginOpenIdResponse.builder().errorCode(ThreadLocalRandom.current().nextInt()).build())
+            .build());
   }
 }
