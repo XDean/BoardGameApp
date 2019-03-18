25b11007106c523539b25c55dc86cc065c7c9515
XDean <373216024@qq.com>
2019-03-18 17:20:41 +0800

    add exception handler

--- mini-boardgame-server/mini-boardgame-server-app/src/main/java/xdean/mini/boardgame/server/endpoint/ExceptionEndPoint.java
+++ mini-boardgame-server/mini-boardgame-server-app/src/main/java/xdean/mini/boardgame/server/endpoint/ExceptionEndPoint.java
@@ -1,8 +1,11 @@
 package xdean.mini.boardgame.server.endpoint;
 
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import xdean.mini.boardgame.server.model.exception.MiniBoardgameException;
 import xdean.mini.boardgame.server.model.param.IllegalArgumentResponse;
 
 @RestControllerAdvice
@@ -11,5 +14,10 @@ public class ExceptionEndPoint {
   @ExceptionHandler(IllegalArgumentException.class)
   public IllegalArgumentResponse handle(IllegalArgumentException e) {
     return new IllegalArgumentResponse(e.getMessage());
+  }
+
+  @ExceptionHandler(MiniBoardgameException.class)
+  public ResponseEntity<?> handle(MiniBoardgameException e) {
+    return ResponseEntity.status(e.getCode()).body(JSONObject.quote(e.getMessage()));
   }
 }
--- mini-boardgame-server/mini-boardgame-server-common/src/main/java/xdean/mini/boardgame/server/model/exception/MiniBoardgameException.java
+++ mini-boardgame-server/mini-boardgame-server-common/src/main/java/xdean/mini/boardgame/server/model/exception/MiniBoardgameException.java
@@ -3,8 +3,14 @@ package xdean.mini.boardgame.server.model.exception;
 import org.springframework.http.HttpStatus;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
 
 @Builder
+@Value
+@ToString(callSuper = false)
+@EqualsAndHashCode(callSuper = false)
 public class MiniBoardgameException extends RuntimeException {
   HttpStatus code;
   String message;
