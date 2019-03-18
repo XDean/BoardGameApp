a8143a16ba83a6c55e25a61568be6c2057ecb1b6
XDean <373216024@qq.com>
2019-03-18 09:20:46 +0800

    add markdown render

---
+++ mini-boardgame-server/mini-boardgame-server-common/src/main/java/xdean/mini/boardgame/server/mvc/MarkdownView.java
@@ -0,0 +1,40 @@
+package xdean.mini.boardgame.server.mvc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.pegdown.PegDownProcessor;
+import org.springframework.web.servlet.view.AbstractTemplateView;
+
+public class MarkdownView extends AbstractTemplateView {
+  @Override
+  protected void renderMergedTemplateModel(
+      Map<String, Object> model,
+      HttpServletRequest request,
+      HttpServletResponse response) throws Exception {
+
+    PrintWriter writer = response.getWriter();
+    writer.append("<html><body>");
+    writer.append(getHtmlFromMarkdown());
+    writer.append("</body></html>");
+  }
+
+  private String getHtmlFromMarkdown() throws URISyntaxException, IOException {
+    String templatePath = "static/" + getUrl();
+    URL templateUrl = MarkdownView.class.getClassLoader().getResource(templatePath);
+    Path path = Paths.get(templateUrl.toURI());
+
+    String markdown = new String(Files.readAllBytes(path));
+
+    return new PegDownProcessor().markdownToHtml(markdown);
+  }
+}
---
+++ mini-boardgame-server/mini-boardgame-server-common/src/main/java/xdean/mini/boardgame/server/mvc/MvcConfig.java
@@ -0,0 +1,33 @@
+package xdean.mini.boardgame.server.mvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
+  @Bean
+  public ViewResolver markdownResolver() {
+    return new AbstractTemplateViewResolver() {
+      {
+        setViewClass(requiredViewClass());
+        setViewNames("*.md");
+      }
+
+      @Override
+      protected Class<?> requiredViewClass() {
+        return MarkdownView.class;
+      }
+    };
+  }
+}
