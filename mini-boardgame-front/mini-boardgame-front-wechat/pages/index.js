2070bade4f2ab85e5540d7476b6e6bd5e8a9facd
XDean <373216024@qq.com>
2019-03-21 17:57:43 +0800

    wechat login and cookie

--- mini-boardgame-front/mini-boardgame-front-wechat/pages/index.js
+++ mini-boardgame-front/mini-boardgame-front-wechat/pages/index.js
@@ -1,5 +1,3 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
@@ -12,13 +10,10 @@ Page({
     if (app.globalData.userInfo) {
       // user info is ready
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.updateUserInfo(res.userInfo)
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           this.updateUserInfo(res.userInfo)
@@ -29,7 +24,6 @@ Page({
   getUserInfo: function(e) {
     this.updateUserInfo(e.detail.userInfo)
   },
-  //事件处理函数
   createGame: function() {
     wx.navigateTo({
       url: 'game-center/create-game'
--- mini-boardgame-front/mini-boardgame-front-wechat/utils/util.js
+++ mini-boardgame-front/mini-boardgame-front-wechat/utils/util.js
@@ -1,3 +1,7 @@
+const serverUrl = "https://xdean.cn:8081/"
+var accessToken = null
+var sessionId = null
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -14,6 +18,36 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const request = input => {
+  wx.request({
+    url: serverUrl + input.url,
+    data: input.data,
+    header: {
+      ...input.header,
+      ...(accessToken && {
+        "Authorization": accessToken
+      }),
+      ...(sessionId && {
+        "Cookie": `JSESSIONID = ${sessionId}`
+      })
+    },
+    method: (input.method ? input.method : 'GET'),
+    success: function(e) {
+      console.log(e)
+      var find = e.cookies.find(x => x.name === 'access-token')
+      accessToken = find && find.value
+      find = e.cookies.find(x => x.name === 'JSESSIONID')
+      sessionId = find && find.value
+      if (input.success)
+        input.success(e)
+    },
+    fail: input.fail,
+    complete: input.complete
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  serverUrl: serverUrl,
+  request: request
 }
