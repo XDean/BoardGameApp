6021479070068c24c6ab8e308726cb62570fd785
XDean <373216024@qq.com>
2019-03-22 13:07:55 +0800

    waiting room, import tooltip

--- mini-boardgame-front/mini-boardgame-front-wechat/pages/game-center/waiting-room.js
+++ mini-boardgame-front/mini-boardgame-front-wechat/pages/game-center/waiting-room.js
@@ -1,15 +1,18 @@
 // pages/game-center/waiting-room.js
 import * as util from '../../utils/util.js'
+import * as tooltip from '../../utils/toolTip.js'
 
 const app = getApp()
 
 Page({
   data: {
     room: {},
-    gameName: {}
+    gameName: {},
+    seatList: []
   },
   onLoad: function() {
     var self = this
+    tooltip.init(this)
     util.request({
       url: 'game/room',
       success: function(e) {
@@ -18,6 +21,7 @@ Page({
           room: e.data.room,
           gameName: app.globalData.gameList.find(x => x.id == e.data.room.gameName).name
         })
+        self.updateSeatList()
       },
       badRequest: function(e) {
         wx.navigateBack()
@@ -33,9 +37,23 @@ Page({
       url: 'game/room/exit',
       method: 'POST',
       data: '{}',
-      complete: function(){
+      complete: function() {
         wx.hideLoading()
       }
     })
+  },
+  updateSeatList() {
+    var room = this.data.room
+    var seatList = []
+    for (var i = 0; i < room.playerCount; i++) {
+      seatList[i] = room.players.find(x => x.seat == i)
+    }
+    this.setData({
+      seatList: seatList
+    })
+  },
+  clickIcon(e) {
+    var player = this.data.room.players[e.currentTarget.id]
+    tooltip.showToolTip('info', player.profile.nickname, 2000);
   }
 })
