47cd8d144d33420102aea6fbcd01ae752975a694
XDean <373216024@qq.com>
2019-03-25 13:12:43 +0800

    refactor socket connect

--- mini-boardgame-front/mini-boardgame-front-wechat/utils/util.js
+++ mini-boardgame-front/mini-boardgame-front-wechat/utils/util.js
@@ -2,7 +2,6 @@ const serverUrl = "https://xdean.cn:8081/"
 const socketUrl = "wss://xdean.cn:8081/"
 var accessToken = null
 var sessionId = null
-var sockets = []
 
 const request = input => {
   if (accessToken)
@@ -52,136 +51,146 @@ const request = input => {
 }
 
 const connectSocket = input => {
-  var url = input.url
-  var cache = sockets.find(x => x.url == url)
-  if (cache) {
-    cache.handle++;
-    return cache.socket
-  }
-  var originSocket = wx.connectSocket({
-    ...input,
-    url: socketUrl + url
-  })
-  var socket = wrapSocket(originSocket)
-  sockets.push({
-    url: url,
-    handle: 1,
-    socket: socket
-  })
+  var socket = wrapSocket(input)
   return socket
 }
 
-const wrapSocket = originSocket => {
-  var authenticated = false
-  var header = null
-  var onOpens = []
-  var onMessages = []
-  var messageBeforeAuth = []
-  var responseFunctions = {}
-  var requestId = 0
-  var lastMsgMillis = new Date().getTime()
-  var heartBeatId = 0
-  originSocket.onOpen(e => {
-    header = e
-    var msg = JSON.stringify({
-      id: ++requestId,
-      topic: 'AUTHENTICATION',
-      attributes: {
-        ACCESS_TOKEN: accessToken
-      },
-      payload: null
-    })
-    originSocket.send({
-      data: msg
-    })
-  })
-  originSocket.onMessage(e => {
-    lastMsgMillis = new Date().getTime()
-    var msg = JSON.parse(e.data)
-    console.log('recieve', msg)
-    switch (msg.topic) {
-      case 'AUTHENTICATION':
-        authenticated = true
-        onOpens.forEach(x => x(header))
-        messageBeforeAuth.forEach(x => {
-          console.log('send', msg)
-          originSocket.send({
-            data: msg
-          })
-        })
-        messageBeforeAuth = []
-        break
-      case 'BAD_CREDENTIAL':
-      case 'WRONG_FORMAT':
-      case 'ERROR':
-        console.error(msg)
-        break
-      case 'ROOM_CANCEL':
-        originSocket.close({
-          reason: 'Room canceled'
-        })
-        break;
-      default:
-        if (msg.id) {
-          var responseFunc = responseFunctions[msg.id]
-          if (responseFunc) {
-            responseFunctions.delete(msg.id)
-            responseFunc(msg)
-          }
-        }
-        onMessages.forEach(x => x(msg))
-    }
-  })
-  originSocket.onError(x => {
-    console.log('error', x)
-  })
-  originSocket.onClose(x => {
-    console.log('close', x)
-    clearInterval(heartBeatId)
-  })
+const wrapSocket = input => {
+  var data = {
+    closed: false,
+    authenticated: false,
+    header: null,
+    onOpens: [],
+    onMessages: [],
+    onErrors: [],
+    onCloses: [],
+    messageBeforeAuth: [],
+    responseFunctions: {},
+    requestId: 0,
+    lastMsgMillis: new Date().getTime(),
+    heartBeatId: 0,
+    originSocket: null,
+  }
   var socket = {
-    ...originSocket,
-    send: e => {
+    send: function(e) {
       var msg = JSON.stringify({
-        id: ++requestId,
+        id: ++data.requestId,
         attributes: {},
         payload: null,
         ...e
       })
-      if (authenticated) {
+      if (data.authenticated) {
         console.log('send', msg)
-        originSocket.send({
+        data.originSocket.send({
           data: msg
         })
       } else {
-        messageBeforeAuth.push(msg)
+        data.messageBeforeAuth.push(msg)
       }
       return {
-        onResponse: func => responseFunctions[msg.id] = func
+        onResponse: func => data.responseFunctions[msg.id] = func
       }
     },
-    onOpen: e => onOpens.push(e),
-    onMessage: e => onMessages.push(e)
-  }
-  heartBeatId = setInterval(() => {
-    var current = new Date().getTime()
-    if (current - lastMsgMillis > 15000) {
-      socket.send({
-        topic: 'HEART_BEAT',
-        attributes: {
-          TIMESTAMP: current
-        }
-      }).onResponse(msg => {
-        if (msg && msg.topic == 'HEART_BEAT') {
-          var serverTime = msg.attributes.TIMESTAMP
-          if (serverTime - current > 5000)
-            console.warn('Heart beat response too slow', socket)
-        } else {
-          console.error('Heart beat error', msg)
+    onOpen: x => data.onOpens.push(x),
+    onMessage: x => data.onMessages.push(x),
+    onError: x => data.onErrors.push(x),
+    onClose: x => data.onCloses.push(x),
+    close: function() {
+      data.originSocket.close()
+    },
+    connect: function() {
+      var originSocket = wx.connectSocket({
+        ...input,
+        url: socketUrl + input.url
+      })
+      data.originSocket = originSocket
+      originSocket.onOpen(e => {
+        data.header = e
+        var msg = JSON.stringify({
+          id: ++data.requestId,
+          topic: 'AUTHENTICATION',
+          attributes: {
+            ACCESS_TOKEN: accessToken
+          },
+          payload: null
+        })
+        originSocket.send({
+          data: msg
+        })
+      })
+      originSocket.onMessage(e => {
+        data.lastMsgMillis = new Date().getTime()
+        var msg = JSON.parse(e.data)
+        console.log('recieve', msg)
+        switch (msg.topic) {
+          case 'AUTHENTICATION':
+            data.authenticated = true
+            data.onOpens.forEach(x => x(data.header))
+            data.messageBeforeAuth.forEach(x => {
+              console.log('send', msg)
+              data.originSocket.send({
+                data: msg
+              })
+            })
+            data.messageBeforeAuth = []
+            break
+          case 'BAD_CREDENTIAL':
+          case 'WRONG_FORMAT':
+          case 'ERROR':
+            console.error(msg)
+            break
+          case 'ROOM_CANCEL':
+            data.closed = true
+            data.originSocket.close({
+              reason: 'Room canceled'
+            })
+            break;
+          default:
+            if (msg.id) {
+              var responseFunc = data.responseFunctions[msg.id]
+              if (responseFunc) {
+                data.responseFunctions.delete(msg.id)
+                responseFunc(msg)
+              }
+            }
+            data.onMessages.forEach(x => x(msg))
         }
       })
+      originSocket.onError(x => {
+        console.log('error', x)
+        onErrors.forEach(e => e(x))
+        originSocket.close()
+      })
+      originSocket.onClose(x => {
+        console.log('close', x)
+        onCloses.forEach(e => e(x))
+        clearInterval(data.heartBeatId)
+      })
+      this.heartBeat()
+    },
+    heartBeat: function() {
+      data.heartBeatId = setInterval(() => {
+        var current = new Date().getTime()
+        if (current - data.lastMsgMillis > 15000) {
+          this.send({
+            topic: 'HEART_BEAT',
+            attributes: {
+              TIMESTAMP: current
+            }
+          }).onResponse(msg => {
+            if (msg && msg.topic == 'HEART_BEAT') {
+              var serverTime = msg.attributes.TIMESTAMP
+              if (serverTime - current > 5000)
+                console.warn('Heart beat response too slow', socket)
+            } else {
+              console.error('Heart beat error', msg)
+            }
+          })
+        }
+      }, 15000)
     }
-  }, 15000)
+  }
+  socket.connect()
   return socket
 }
 
