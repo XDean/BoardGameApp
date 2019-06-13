9ab052b8fa974d66a1cbb53afe0e2c40aba97603
dxu <dean.xu@asml.com>
2019-06-13 16:18:17 +0800

    use xecho.validator

--- mini-boardgame-server/mini-boardgame-server-go/web/handler/validator.go
+++ mini-boardgame-server/mini-boardgame-server-go/web/handler/validator.go
@@ -1,54 +1,17 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/dlclark/regexp2"
 	"github.com/labstack/echo/v4"
-	"gopkg.in/go-playground/validator.v9"
-	"net/http"
-	"reflect"
+	"github.com/xdean/goex/xecho"
 )
 
-type Validator struct {
-	Validator *validator.Validate
-}
-
-func (cv *Validator) Validate(i interface{}) error {
-	if err := cv.Validator.Struct(i); err == nil {
-		return nil
-	} else {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-}
-
-func NewValidator() *Validator {
-	v := validator.New()
-	_ = v.RegisterValidation("regexp", validRegexp)
-	return &Validator{Validator: v}
-}
-
-var REGEX = map[string]*regexp2.Regexp{
+var Regexps = map[string]*regexp2.Regexp{
 	"USERNAME": regexp2.MustCompile("^(?!_)(?![0-9]+$)[a-zA-Z0-9_]+(?<!_)$", 0),
 	"PASSWORD": regexp2.MustCompile("^(?![0-9]+$)(?![a-zA-Z]+$)(?=[A-Za-z])[0-9A-Za-z]{6,16}$", 0),
 }
 
-func validRegexp(fl validator.FieldLevel) bool {
-	field := fl.Field()
-	param := fl.Param()
-
-	switch field.Kind() {
-	case reflect.String:
-		str := field.String()
-		if str == "" {
-			return true
-		}
-		if regex, find := REGEX[param]; find {
-			isMatch, _ := regex.MatchString(str)
-			return isMatch
-		} else {
-			panic(fmt.Sprintf("Bad regex key %v", str))
-		}
-	default:
-		panic(fmt.Sprintf("Bad field type %T", field.Interface()))
-	}
+func NewValidator() echo.Validator {
+	xecho.ValidateRegexps = Regexps
+	return xecho.NewValidator()
 }
