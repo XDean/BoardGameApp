2ae4a7dd5cac8a40a4c681e412f9bd9b657d9709
dxu <dean.xu@asml.com>
2019-07-04 08:19:46 +0800

    find hu

--- others/fan/fan/hu.go
+++ others/fan/fan/hu.go
@@ -1,27 +1,35 @@
 package fan
 
-type (
-	Hu func(hand Hand) []GroupHand
-)
-
-var HU_COMMON = func(hand Hand) []GroupHand {
+func FindHu(hand Hand) []GroupHand {
 	result := make([]GroupHand, 0)
+	for _, groups := range findGroup(hand.Private) {
+		result = append(result, GroupHand{
+			Groups: groups,
+			Last:   hand.Last,
+			ZiMo:   hand.ZiMo,
+		})
+	}
+	return result
+}
 
-	for card, _ := range hand.Private {
+func findGroup(cards Cards) [][]Group {
+	for card, _ := range cards {
 		for _, t := range []GroupType{SHI_SAN_YAO, QI_XING_BU_KAO, QUAN_BU_KAO, QI_DUI} {
-			if ok, group, _ := t.Find(hand.Private, card); ok {
-				return []GroupHand{
-					{
-						Groups: []Group{group},
-						Last:   hand.Last,
-						ZiMo:   hand.ZiMo,
-					},
+			if ok, group, _ := t.Find(cards, card); ok {
+				return [][]Group{{group}}
+			}
+		}
+		for _, t := range []GroupType{ZU_HE_LONG, KE, SHUN} {
+			if ok, group, left := t.Find(cards, card); ok {
+				leftGroups := findGroup(left)
+				for i, g := range leftGroups {
+					leftGroups[i] = append(g, group)
 				}
+				return leftGroups
 			}
 		}
 
 		break
 	}
-
-	return result
+	return nil
 }
