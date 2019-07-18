cb492fddc05f1973335d155f5a2301a8d73acc79
Dean Xu <373216024@qq.com>
2019-07-18 21:44:25 +0800

    calc fan

--- go/wechat/handler/message.go
+++ go/wechat/handler/message.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/xdean/goex/xecho"
+	"github.com/xdean/miniboardgame/go/games/guobiao/guobiao"
 	"github.com/xdean/miniboardgame/go/wechat/model"
 	"net/http"
 	"time"
@@ -13,13 +14,17 @@ func Message(c echo.Context) error {
 	param := new(model.Message)
 	xecho.MustBindAndValidate(c, param)
 
-	fmt.Println(param)
+	hand, err := guobiao.Parse(param.Content)
+	if err != nil {
+		return err
+	}
+	fan := guobiao.CalcFan(hand)
 
 	return c.XML(http.StatusOK, model.Message{
 		FromUserName: param.ToUserName,
 		ToUserName:   param.FromUserName,
 		CreateTime:   time.Now().Unix(),
-		Content:      "echo: " + param.Content,
+		Content:      fmt.Sprintf("番型: %s", fan),
 		MsgType:      model.TEXT,
 	})
 }
