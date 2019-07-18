417ee47c95fe925c9b8af7f4d52f1b337be0bf0c
Dean Xu <373216024@qq.com>
2019-07-18 19:00:16 +0800

    use cli instead of flag

--- go/wechat/main.go
+++ go/wechat/main.go
@@ -1,33 +1,58 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
 	"github.com/xdean/goex/xecho"
+	"github.com/xdean/miniboardgame/go/wechat/config"
 	"github.com/xdean/miniboardgame/go/wechat/handler"
 	"net/http"
 	"os"
 )
-import "github.com/xdean/miniboardgame/go/wechat/config"
 
 func main() {
-	flag.StringVar(&config.SettingFile, "setting", "", "setting file path")
-	flag.StringVar(&config.SettingFile, "secret-key", "123456", "secret key")
 
-	flag.Parse()
-	if config.SettingFile == "" {
-		fmt.Println("Please specify setting file")
-		os.Exit(1)
+	app := cli.NewApp()
+
+	app.Name = "Wechat BG"
+	app.Usage = "Run XDean Wechat BG Server"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "setting,s",
+			Usage:       "Setting file path",
+			Destination: &config.SettingFile,
+		},
+		cli.StringFlag{
+			Name:        "key,k",
+			Usage:       "Secret key",
+			Destination: &config.SecretKey,
+		},
 	}
-	err := config.Instance.Load(config.SettingFile)
+
+	app.Action = func(c *cli.Context) error {
+		if config.SettingFile == "" {
+			return errors.New("Please specify setting file")
+		}
+		err := config.Instance.Load(config.SettingFile)
+		if err != nil {
+			return err
+		}
+		run()
+		return nil
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println("Fail to load setting file: ", err.Error())
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
+}
+func run() {
 	e := echo.New()
 	e.Validator = xecho.NewValidator()
 
