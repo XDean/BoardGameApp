62fb19b5287dd8289c18a5161aa5050b075fd2af
dxu <dean.xu@asml.com>
2019-09-09 16:51:46 +0800

    game model

--- go/server/game/game.go
+++ go/server/game/game.go
@@ -1,5 +1,41 @@
 package game
 
-type GameProvider interface {
-	Name() string
+type (
+	Game struct {
+		Id   string
+		Name string
+
+		// setup
+		Player  Range
+		Options []Option
+
+		// play
+		NewEvent func() interface{}
+		OnEvent  func(interface{}) (interface{}, error)
+	}
+
+	Option struct {
+		Id     string
+		Name   string
+		Type   OptionType
+		Domain interface{}
+	}
+
+	OptionType int
+
+	Range struct {
+		Min int
+		Max int
+	}
+
+	Event struct {
+		PlayerId int
+		RoomId   int
+	}
+)
+
+var Games []*Game
+
+func Register(game *Game) {
+	Games = append(Games, game)
 }
